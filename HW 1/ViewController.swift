208dd091332ad4fe05597cafb14bdf123fc9c41d
marktufano <marktufano@gmail.com>
2016-02-02 18:21:05 -0500

    Initial Commit

---
+++ HW 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW 1
+//
+//  Created by Mark Tufano on 2/2/16.
+//  Copyright © 2016 Mark Tufano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
